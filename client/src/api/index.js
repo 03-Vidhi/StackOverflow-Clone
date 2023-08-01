1a2de150cec10f15a785645d2e6ef008bdd00a2f
Vidhi Gupta <121597033+03-Vidhi@users.noreply.github.com>
2023-08-01 19:06:25 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://stack-overflow-clone-ashen.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
